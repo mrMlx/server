7de7ed1ecea6ae03f0ec3bb746b0c4cddbf9be9e
mrMlx <maks.loz5@mail.ru>
2024-05-06 16:40:05 +0300

    Update server.py

--- server.py
+++ server.py
@@ -4,20 +4,10 @@ HDRS = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n".encod
 
 socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-isActive = False
-for i in range(0, 256):
-    if isActive == True:
-        i -= 1
-        break
-    for j in range(0, 256):
-        try:
-            socket_server.bind((f"192.168.{i}.{j}", 12345))
-            isActive = True
-            break
-        except:
-            pass
-socket_server.listen(1)
-print(f"192.168.{i}.{j}\nlistening")
+
+socket_server.bind(("192.168.0.54", 12345)) # your ip and port
+
+print("listening")
 
 socket_client, address_client = socket_server.accept()
 print("connected:", address_client)
@@ -30,4 +20,4 @@ content = HDRS + data.encode("utf-8")
 socket_client.send(content)
 
 socket_client.close()
-#socket_server.close()
+socket_server.close()
