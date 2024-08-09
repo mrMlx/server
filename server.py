15a8ea3bbadef0ca5e31c5f8f2e7e322421e4497
mrMlx <maks.loz5@mail.ru>
2024-08-09 16:30:17 +0300

    Update server.py

--- server.py
+++ server.py
@@ -4,8 +4,11 @@ HDRS = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n".encod
 
 socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-
-socket_server.bind(("192.168.0.54", 12345)) # your ip and port
+localIP = "192.168.0.54"
+port = 12345
+socket_server.bind((localIP, port)) # your ip and port
+print(f"local IP {localIP} "
+      f"port {port}")
 
 socket_server.listen(4)
 print("listening")
